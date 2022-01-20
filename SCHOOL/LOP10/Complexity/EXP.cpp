6609f065ad469c500d2f7aeffb3da88d87b091f5
rubikshieu <rubikshieutv@gmail.com>
2022-01-20 21:49:26 +0700

    Stack and Queue 18 - 1 - 2022

---
+++ SCHOOL/LOP10/Complexity/EXP.cpp
@@ -0,0 +1,11 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    freopen("EXP.INP", "r", stdin);
+    freopen("EXP.OUT", "w", stdout);
+    double n;
+    cin >> n;
+    cout << fixed << setprecision(10) << exp(n);
+    return 0;
+}
