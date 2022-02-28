c8d106c554c714bd3e2830ed29aea002dedfb179
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2022-02-28 17:07:02 +0700

    28-2 Sync

--- test/test.cpp
+++ test/test.cpp
@@ -1,30 +1,21 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-int main() {
-    int n = 1234;
-    int sum = 0;
-    while(n > 0){
-        sum = sum + n % 10;
-        n = n / 10;
+int main(){
+    int left = 1;
+    int right = 10;
+    int check = 5;
+    int a[11] = {0,1,2,3,3,4,5,6,7,8,9};
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if (a[mid] < check){
+                left = mid + 1;
+        }
+        else{
+            right = mid - 1;
+        }
     }
-    cout << sum;
+    cout << left;
+
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
