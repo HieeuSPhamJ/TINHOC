e02d3d82dd0b9e9420490112dd8ddfc4184fcb67
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2022-05-02 00:14:44 +0700

    H prob

---
+++ ONLINE OJ/CODEFORCES/Contest kho dam/Codeforces Round #784 div 4/H.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+int main(){ 
+    int test;
+    cin >> test;
+    while(test--){
+        int n, k;
+        cin >> n >> k;
+        int countBit[33];
+        for (int i = 0; i <= 33; i++){
+            countBit[i] = 0;
+        }
+
+        for (int i = 1; i <= n; i++){
+            int inp;
+            cin >> inp;
+            int e = 31;
+            while(inp > 0){
+                countBit[e] += inp % 2;
+                inp /= 2;
+                e--;
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= 31; i++){
+            // cout << countBit[i];
+            if (n - countBit[i] <= k){
+                k = k - (n - countBit[i]);
+                // cout << (n - countBit[i]);
+                ans += pow(2, 31 - i);
+            }
+        }
+        // cout << endl;
+        cout << ans << endl;
+
+    }
+
+    return 0;
+}
--- ONLINE OJ/Hackerrank/LastAprilTesting/E.cpp
+++ ONLINE OJ/Hackerrank/LastAprilTesting/E.cpp
@@ -1,30 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int dp[100010];
+
 int main(){
     int test;
     cin >> test;
     while (test--){
-        int a, b, c;
-        cin >> a >> b >> c;
-        if (a % 2){
-            if (b * 2 < c){
-                cout << b * a;
+        int a, b, n;
+        cin >> n >> a >> b;
+        dp[1] = a;
+        for (int i = 2; i <= n; i++){
+            if (i % 2 == 0){
+                dp[i] = min(dp[i - 1] + a, dp[i / 2] + b);
             }
             else{
-                cout << c * (a / 2);
+                dp[i] = dp[i - 1] + a;
             }
         }
-        else{
-            a = a - 1;
-            if (b * 2 < c){
-                cout << b * a + b;
-            }
-            else{
-                cout << c * (a / 2) + b;
-            }
-        }
-
+        cout << dp[n];
         cout << endl;
     }
 
