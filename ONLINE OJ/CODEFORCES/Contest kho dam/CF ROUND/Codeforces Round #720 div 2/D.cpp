608fca40bb05d31740c6b3bfad294a415a1568c5
Hiếu Phạm Đình Trung <hieuphamdinhtrung@192.168.2.3>
2022-12-20 19:25:03 +0700

    Dadasaads

---
+++ ONLINE OJ/CODEFORCES/Contest kho dam/CF ROUND/Codeforces Round #720 div 2/D.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+#define ii pair <int,int>
+#define fi first
+#define se second
+#define int long long
+#define double long double
+#define endl '\n'
+using namespace std;
+
+signed main(){
+    //freopen("input.INP", "r", stdin);
+    //freopen("output.OUT", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int test;
+    cin >> test;
+    while(test--){
+        int n;
+        cin >> n;
+        for (int i = 1; i < n; i++){
+
+        }
+    }
+    return 0;
+}
