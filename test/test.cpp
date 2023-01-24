8c95b4271ee431c480a9913bcebce7069f365b61
Hiếu Phạm Đình Trung <hieuphamdinhtrung@Hieus-MacBook-Air.local>
2023-01-25 00:24:37 +0700

    adadasd

--- test/test.cpp
+++ test/test.cpp
@@ -7,31 +7,56 @@
 #define endl '\n'
 using namespace std;
 
-signed main(){
-    //freopen("input.INP", "r", stdin);
-    //freopen("output.OUT", "w", stdout);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int test;
-    cin >> test;
-    while(test--){
-        int n;
-        cin >> n;
-        vector <int> s;
-        for (int i = 1; i <= n; i++){
-            int x;
-            cin >> x;
-            s.push_back(x);
+const int N = 1e5 + 5;
+int n , m , q;
+
+ii a[N][12];
+int b[N][12];
+
+signed main()
+{
+    //freopen("D.inp","r",stdin);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> q;
+    while(q--)
+    {
+        cin >> n >> m;
+        set < int > s;
+        for (int i = 1; i <= n; i++)
+        {
+            for (int j= 1; j <= m; j++)
+            {
+                cin >> b[i][j];
+                a[i][j].fi = b[i][j];
+                a[i][j].se = j;
+            }
+            sort(a[i] + 1 , a[i] + 1 + m);
+            int x = 0;
+            for (int j= 1; j <= m; j++)
+            {
+                x = x * 10 + a[i][j].se;
+                s.insert(x);
+            }
         }
-        if (!next_permutation(s.begin(), s.end())){
-            cout << -1 << endl;
-            continue;
-        }
-        for (auto i: s){
-            cout << i;
+        for (int i = 1; i <= n; i++)
+        {
+            int x = 0;
+            for (int j= 1; j <= m + 1; j++)
+            {
+                if(j == m + 1)
+                {
+                    cout << j - 1 << " ";
+                    break;
+                }
+                x = x * 10 + b[i][j];
+                if(s.find(x) == s.end())
+                {
+                    cout << j - 1 << " ";
+                    break;
+                }
+            }
         }
         cout << endl;
     }
-    return 0;
 }
