bcbc4bf99924bf0d6ede6276b38506b9ab8e13b7
Pham Dinh Trung Hieu <phamdinhtrunghieu1103@gmail.com>
2023-03-26 23:38:21 +0700

    dung3213

--- SCHOOL/LOP 11/Lists - python/Banhang.py
+++ SCHOOL/LOP 11/Lists - python/Banhang.py
@@ -5,8 +5,8 @@ ans = 10**10
 while n:
     n -= 1
     
-    p, v, d = map(int, input().split())
-    print(ans, p + v * c + d)
+    p, d, v = map(int, input().split())
+    # print(ans, p + v * c + d)
     ans = min(ans, p + v * c + d)
     
 print(ans)
