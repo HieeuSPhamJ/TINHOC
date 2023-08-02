ccdad3556f7f2c7ec03cb39d845f1a95bcf7ebf0
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2023-08-03 00:18:48 +0700

    dasdadA

--- ONLINE OJ/CODEFORCES/Contest kho dam/EDU ROUND/Educational Codeforces Round 48 div 2/E.cpp
+++ ONLINE OJ/CODEFORCES/Contest kho dam/EDU ROUND/Educational Codeforces Round 48 div 2/E.cpp
@@ -10,11 +10,43 @@ using namespace std;
 
 const int maxN = 2e5 + 10;
 
+int n;
 ii a[maxN];
 int p[maxN];
 
 double len(ii x, ii y){
     return sqrt((x.fi - y.fi) * (x.fi - y.fi) + (x.se - y.se) * (x.se - y.se));
+}
+
+double calx(int x, int y, int A, int s){
+    if (x == A){
+        return x;
+    }
+    double res = (- (s - y)) * x + (A - x) * y;
+    res /= (- (s - y));
+    return res;
+}
+
+double cal(double x){
+    int l = 1;
+    int r = n + 1;
+    int res = 0;
+    while(l <= r){
+        int i = (l + r) / 2;
+        if (a[i].fi <= x and x <= a[i].se){
+            return p[i - 1] + (x - a[i].fi);
+        }
+        if (a[i - 1].se <= x and x <= a[i].fi){
+            return p[i - 1];
+        }
+        if (a[i].se < x){
+            l = i + 1;
+        }
+        else{
+            r = i - 1;
+        }
+        
+    }
 }
 
 signed main(){
@@ -26,9 +58,10 @@ signed main(){
     int s, A, B;
     cin >> s >> A >> B;
     int base = B - A;
-    int n;
+    
     cin >> n;
     a[0] = {0,0};
+    a[n + 1] = {1e17, 1e18};
     for (int i = 1; i <= n; i++){
         cin >> a[i].fi >> a[i].se;
         p[i] = p[i - 1] + a[i].se - a[i].fi;
@@ -38,9 +71,21 @@ signed main(){
     while(test--){
         int x, y;
         cin >> x >> y;
-        int k = 1.0 * (y - s) / y;
-        int l = x - y * 1.0 * (x - A) / (y - s);
-        int r = x + 
+        double k = 1.0 * (y - s) / y;
+        
+        double l = calx(x,y,A,s);
+        double r = calx(x,y,B,s);
+        // cout << l << " " << r << endl;
+        double cover = cal(r) - cal(l);
+        // cout << cover << " " << (y - s) << endl;
+        cout << fixed << setprecision(15) << cover * k << endl;
     }
+
+    // cout << cal(3,1,1,-3);
     return 0;
 }
+
+/*
+123456789012345
+000000000000000
+*/
