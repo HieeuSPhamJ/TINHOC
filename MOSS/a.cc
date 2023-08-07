9385873b5a6d52fbdce6567e22c090a4996054b7
Hiếu Phạm Đình Trung <hieuphamdinhtrung@Hieus-MacBook-Air.local>
2023-08-07 22:13:59 +0700

    Dadsada

--- MOSS/a.cc
+++ MOSS/a.cc
@@ -8,41 +8,14 @@
 #define all(x) x.begin(), x.end()
 using namespace std;
 
-const int maxN = 2000;
+const int maxN = 2e6 + 10;
 
-    int n, k;
 int a[maxN];
+int b[maxN];
 
-int check(int x){
-    for (int i = 1; i < n; i++){
-        // cout << "With: " << i << endl;
-        if (a[i] >= x){
-            // cout << "lon" << endl;
-            return 1;
-        }
-        int t = 0;
-        int xx = x;
-        for (int j = i; j <= n; j++){
-            // cout << j << ": "<< xx - a[j] << endl;
-            if (max(0ll, xx - a[j]) == 0){
-                break;
-            }
-            if (j == n){
-                t = 1e18;
-                break;
-            }
-            t += max(0ll, xx - a[j]);
-            if (t > k){
-                break;
-            }
-            xx--;
-        }
-        if (t <= k){
-            // cout << "ok at " << i << " " << t << endl;
-            return 1;
-        }
-    }
-    return a[n] >= x;
+
+bool cmp(int a, int b){
+    return a > b;
 }
 
 signed main(){
@@ -54,29 +27,25 @@ signed main(){
     int test;
     cin >> test;
     while(test--){
-        cin >> n >> k;
+        int n;
+        cin >> n;
+        int m = n * (n - 1) / 2;
+        for (int i = 1; i <= m; i++){
+            cin >> b[i];
+        }
+        sort(b + 1 , b + 1 + m, cmp);
+        a[1] = 1e9;
+        for(int i = 2, sum = 1; i <= n; i++){
+            a[i] = b[sum];
+            sum += i;
+        }
         for (int i = 1; i <= n; i++){
-            cin >> a[i];
+            cout << a[i] << " ";
         }
-
-        int l = *max_element(a + 1, a + 1 + n);
-        int r = 1e18;
-        int res = l;
-
-        while(l <= r){
-            int mid = (l + r) / 2;
-            if (check(mid)){
-                l = mid + 1;
-                res = mid;
-            }
-            else{
-                r = mid - 1;
-            }
-        }
-
-        // cout << check(7) << endl;
-        
-        cout << res << endl;
+        cout << endl;
     }
     return 0;
 }
+
+
+
--- MOSS/b.cc
+++ MOSS/b.cc
@@ -1,99 +1,55 @@
 #include<bits/stdc++.h>
-#define ii pair <int,int>
+using namespace std;
+
+#define y1 as214
+#define ii pair < int , int >
+#define iii pair < int , ii >
+#define iv pair < ii , ii >
+
 #define fi first
 #define se second
+#define fr front()
+#define pb push_back
+#define pp pop_back()
+#define t top()
+#define int int
+
+#define FOR(i , x , n) for(int i = x ; i <= n ; ++i)
+#define REP(i , n) for(int i = 0 ; i < n ; ++i)
+#define FORD(i , x , n) for(int i = x ; i >= n ; --i)
+
+#define oo 1e18
 #define int long long
-#define double long double
-#define endl '\n'
-#define all(x) x.begin(), x.end()
 
-using namespace std;
-const int maxN = 2000;
+const int N = 1e6 + 5;
+int q , n , m , sum;
+int a[N] , b[N] , c[N];
 
-int n, k;
-int a[maxN];
-
-int check(int x)
+main()
 {
-    for (int i = 1; i <= n; ++ i)
+    //freopen("test.inp", "r", stdin);
+	//freopen("1.ans", "w", stdout);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> q;
+    while(q--)
     {
-        if (a[i] >= x)
-        {
-            return 1;
-        }
-    }
-    int cnt;
-    for (int i = 1; i < n; ++ i)
-    {
-        cnt = 0;
-        for (int j = i; j <= n; ++ j)
-        {
-            if (j == n)
-            {
-                if (a[n] >= x - (n - i))
-                {
-                    return 1;
-                }
-                cnt = k + 1;
-                break;  
-            }   
-            if (a[j] >= x - (j - i))
-            {
-                // cout << "**" << i << ' ' << j << endl;
-                return 1;
-            }
-            cnt = cnt + (x - (j - i)) - a[j];
-            if (cnt > k)
-            {
-                break;
-            }
-        }
-        if (cnt > k)
-        {
-            continue;
-        }
-        else
-        {
-            // cout << "***" << i << ' ' << cnt << endl;
-            return 1;
-        }
-    }
-    return 0;
-}
+        cin >> n;
 
-signed main()
-{
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int Test;
-    cin >> Test;
-    for (int TestCase = 1; TestCase <= Test; ++ TestCase)
-    {
-        cin >> n >> k;
-        int l = -1;
-        for (int i = 1; i <= n; i++)
+        int m = n * (n - 1) / 2;
+        FOR(i , 1 , m)
+            cin >> b[i];
+        sort(b + 1 , b + 1 + m , greater < int > ());
+        a[1] = 1e9;
+        int sum = 1;
+        for(int i = 2 ; i <= n ; i++)
         {
-            cin >> a[i];
-            l = max(l, a[i]);
+            a[i] = b[sum];
+            sum += i;
         }
-        int r = 1e18, mid, res = l;
-        while(l <= r)
-        {
-            mid = (l + r) >> 1;
-            if (check(mid))
-            {
-                l = mid + 1;
-                res = mid;
-            }
-            else
-            {
-                r = mid - 1;
-            }
-        }
-        // cout << check(5) << endl;
-        cout << res << endl;
+        FOR(i , 1 , n)
+            cout << a[i] << " ";
+        cout << "\n";
     }
-    return 0;
+
 }
