114ee8a82fe62eb3c9b2e908681e3ec9dd04aa74
hieupham1103 <phamdinhtrunghieu1103@gmail.com>
2023-09-05 23:39:03 +0700

    dsada

--- CodeChecker/A.cpp
+++ CodeChecker/A.cpp
@@ -32,7 +32,6 @@ void dijkstra(){
         int du = pq.top().fi;
         pq.pop();
 
-<<<<<<< HEAD
         if (du > dist[u]) continue;
         for(auto pt: graph[u]){
             int v = pt.fi;
@@ -88,73 +87,4 @@ main()
         timein[spec[i]] = timein[spec[i-1]] + edge[spec[i]][spec[i-1]];
     }
     dijkstra();
-=======
-const int maxN = 2e5 + 10;
-
-int n;
-int a[maxN];
-vector <int> adj[maxN];
-int dp[maxN];
-int dp2[maxN];
-int child[maxN];
-int lvl[maxN];
-
-
-void dfs(int nu, int fa){
-    child[nu] = 1;
-    for (auto i: adj[nu]){
-        if (i == fa){
-            continue;
-        }
-        lvl[i] = lvl[nu] + 1;
-        dfs(i, nu);
-        child[nu] += child[i];
-        dp[nu] += dp[i];
-    }
-    dp[nu] += (child[nu] - 1);
-}
-int cnt = 0;
-void dfs2(int nu, int fa){
-    cnt++;
-    dp2[nu] += dp2[fa];
-    if (fa != nu){
-        for (auto i: adj[fa]){
-            if (i == nu or lvl[i] < lvl[fa]){
-                continue;
-            }
-            dp2[nu] += dp[i];
-        }
-        dp2[nu] += n - (child[nu] + 1);
-    }
-    for (auto i: adj[nu]){
-        if (i == fa){
-            continue;
-        }
-        dfs2(i,nu);
-    }
-}
-
-signed main(){
-    freopen("input.inp", "r", stdin);
-    freopen("A.out", "w", stdout);
-    //freopen("input.INP", "r", stdin);
-    //freopen("output.OUT", "w", stdout);
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin >> n;
-    for (int i = 1; i < n; i++){
-        int a, b;
-        cin >> a >> b;
-        adj[a].push_back(b);
-        adj[b].push_back(a);
-    }
-    dfs(1,1);
-    dfs2(1,1);
-
-    cout << n << endl;
-
-    return 0;
->>>>>>> 504a6cc3a59da46aa77138bf91bb5be96c0834ff
 }
